972591d76cd35d389713e6652bd7f8dad4c3ecb9
pon-ktnrp <pon.ktnrp@gmail.com>
2025-09-09 04:11:57 +0700

    preview sidebar

---
+++ src/app/home/profile/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+export default function ProfilePage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-4xl font-bold mb-4">Profile Page</h1>
+      <p className="text-lg">This is where user profile information will be displayed.</p>
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
 import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 // ฟอนต์ custom
 const petitFormal = localFont({
@@ -33,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${petitFormal.variable} antialiased`}
+        className={`${GeistSans.variable} ${GeistMono.variable} ${petitFormal.variable} antialiased`}
       >
         {children}
       </body>
