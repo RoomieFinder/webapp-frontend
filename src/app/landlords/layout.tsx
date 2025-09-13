a99de7ad38664c79dc800a71bc62d939032f207b
pon-ktnrp <pon.ktnrp@gmail.com>
2025-09-14 02:39:09 +0700

    Create topbar and update sidebar and inprogress profile edit page

--- src/app/landlords/layout.tsx
+++ src/app/landlords/layout.tsx
@@ -1,14 +1,29 @@
 "use client";
 import React from "react";
 import MiniDrawer from "@/components/ui/Sidebar";
+import ExploreIcon from '@mui/icons-material/ExploreTwoTone';
+import ProfileIcon from '@mui/icons-material/Person2TwoTone';
+import FindRoommateIcon from '@mui/icons-material/SearchTwoTone';
+import CreatePartyIcon from '@mui/icons-material/Groups2TwoTone';
+import PaymentIcon from '@mui/icons-material/PaidTwoTone';
 
-export default function LandlordLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const navItems = [
+    { text: "Explore", path: "/tenants",icon: <ExploreIcon/> },
+    { text: "Profile", path: "/tenants/profile", icon: <ProfileIcon /> },
+    { text: "Find Roommate", path: "/tenants/find-roommate", icon: <FindRoommateIcon /> },
+    { text: "Create Party", path: "/tenants/create-party", icon: <CreatePartyIcon /> },
+    { text: "Payment", path: "/tenants/payment", icon: <PaymentIcon /> },
+  ];
+
   return (
-    <div className="flex">
-      <MiniDrawer />
-      <main className="flex-1 bg-[#1D2D44] text-[var(--foreground)]">
-        {children}
-      </main>
-    </div>
+    <html lang="en">
+      <body>
+        <MiniDrawer navItems={navItems} />
+        <main className="flex-1 bg-[#1D2D44] text-[var(--foreground)]">
+          {children}
+        </main>
+      </body>
+    </html>
   );
 }
