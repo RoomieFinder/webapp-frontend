957b2de117ee75cc8b4b702170a093553da80cfe
Piyapanchanok Meeboonsalang <piyapanchanok@gmail.com>
2025-10-18 19:02:53 +0700

    Config pictures & some icon resources

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     domains: [
-      "pub-1def55cca1954841a8ec0f5d5d8cee6d.r2.dev"
+      "pub-1def55cca1954841a8ec0f5d5d8cee6d.r2.dev", "images.unsplash.com",
     ],
   },
 };
